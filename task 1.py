d2289e23b1ed5fae530642e914b430001082b84b
Hamdi Tarek <33429329+hamdiitarek@users.noreply.github.com>
2024-02-29 13:51:45 +0200

    Student Phone Number Data Added

--- task 1.py
+++ task 1.py
@@ -1,8 +1,11 @@
 names = []
 IDs = []
+Phone = []
 num = int(input('Enter number of students : '))
 for x in range(num):
     names.append(input("Enter student's name : "))
     IDs.append(input("Enter student's ID : "))
+    Phone.append(input("Enter student's Phone Number : "))
 print (names)
 print (IDs)
+print (Phone)
