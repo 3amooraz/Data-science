13fca11ed7ab10c1300ce5ef3856233181fbb5bc
3amooraz <omarwaleed1907@gmail.com>
2024-02-29 11:04:03 +0200

    1

---
+++ task 1.py
@@ -0,0 +1,8 @@
+names = []
+IDs = []
+num = int(input('Enter number of students : '))
+for x in range(num):
+    names.append(input("Enter student's name : "))
+    IDs.append(input("Enter student's ID : "))
+print (names)
+print (IDs)
